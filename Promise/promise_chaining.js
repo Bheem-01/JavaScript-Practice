36ec87f819991b88c7e7c57dfc29f4637571c1ca
Bheem <bheemsharma0111@gmail.com>
2026-06-30 16:20:58 +0530

    Added_Promise_chaining

---
+++ Promise/promise_chaining.js
@@ -0,0 +1,18 @@
+// Resolve 5, then double it using Promise Chaining.
+let promise = new Promise((resolve, reject)=>{
+    resolve(5);
+});
+
+promise.then((result)=>{
+    console.log(result);
+    return result * 2;
+})
+
+ .then((result)=>{
+    // resolve(result * 2);
+    console.log(result);
+
+}); /*The moment you write a semicolon (;) in the middle,
+ it's like putting a full stop in the sentence.
+  The chain ends there, so the next .then() has nothing to attach to,
+   which causes the syntax error. */
